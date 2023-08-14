19c41a2d5b04e0cc02491e06010a91f4c12f59d2
fushouli <DeemoWings@outlook.com>
2023-08-14 22:47:20 +0800

    hello,github

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,20 @@
+
 public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("hello,world!");
 
         System.out.println("hello,github!");
+
+        System.out.println("hello,cursor");
+
+        /*------------------------------------*/
+
+        MyClass mc = new MyClass();
+		mc.func();
+
+		/*------------------------------------*/
+
+		
     }
 }
---
+++ MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public MyClass()
+    {
+        System.out.println("this is ctor of MyClass");
+    }
+
+    public void func()
+    {
+        System.out.println("this is a member function.");
+    }
+}
