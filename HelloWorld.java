2ebee1e18e9eadd6db6d87a88e2348127ea511d5
fushouli <DeemoWings@outlook.com>
2023-08-15 21:44:29 +0800

    hello,github

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class HelloWorld {
     public static void main(String[] args)
@@ -15,6 +16,62 @@ public class HelloWorld {
 
 		/*------------------------------------*/
 
-		
+		Enumeration<String> days;
+        Vector<String> dayNames = new Vector<String>();
+        dayNames.add("Sunday");
+        dayNames.add("Monday");
+        days = dayNames.elements();
+
+        while(days.hasMoreElements())
+        {
+            System.out.println(days.nextElement());
+        }
+
+        /*------------------------------------*/
+
+        BitSet bits = new BitSet(10);
+        BitSet bits_ = new BitSet(20);
+        bits.set(5);
+        bits.set(7);
+        bits_.set(13);
+        bits_.set(16);
+        bits.or(bits_);
+        System.out.println(bits.toString());
+
+        /*------------------------------------*/
+
+        Vector<String> vec = new Vector<>();
+        vec.add("first");
+        vec.add(0,"before first");
+        System.out.println(vec.capacity());
+        System.out.println(vec.size());
+        String str = vec.elementAt(1);
+        System.out.println(str);
+
+        /*------------------------------------*/
+
+        Stack<String> stack = new Stack<>();
+        stack.add("1");
+        stack.add("2");
+        stack.push("3");
+        System.out.println("stack: " + stack.toString());
+        System.out.println("stack peek: " + stack.peek());
+        System.out.println("stack search: " + stack.search("2"));
+
+        /*------------------------------------*/
+
+        //Dictionary已过时
+
+        /*------------------------------------*/
+
+        Hashtable<Integer,String> hashtable = new Hashtable<>();
+        hashtable.put(1,"1");
+        hashtable.put(2,"2");
+        hashtable.put(3,"3");
+        System.out.println("hashtable[2]: " + hashtable.get(2));
+
+        /*------------------------------------*/
+
+
     }
 }
