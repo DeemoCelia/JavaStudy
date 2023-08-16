634bfefe79555a03bdda7e9efb44b29df482c9de
fushouli <DeemoWings@outlook.com>
2023-08-16 21:33:33 +0800

    Study - 2023.8.16

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import javax.swing.text.html.Option;
+import java.io.*;
 import java.util.*;
+import java.net.*;
+import java.util.function.Consumer;
 
 public class HelloWorld {
     public static void main(String[] args)
@@ -102,5 +103,86 @@ public class HelloWorld {
 		    System.out.println("configs: " + str);
 	    }
 
+        /*------------------------------------*/
+
+        HelloWorld hw = new HelloWorld();
+        //hw.clientSocketFunc();
+
+        /*------------------------------------*/
+        //JDK 8.0
+
+        MathOperation addition = (int a,int b) -> {return a + b;};
+
+        MathOperation subtraction = (a,b)->a-b;
+
+        MathOperation mutiplicaiton = (int a, int b)->{return a*b;};
+
+        MathOperation division = (int a,int b)->a/b;
+
+        GreetingService greetingService1= msg -> System.out.println("hello "+msg);
+        GreetingService greetingService2 = (msg) -> {
+            System.out.println("hello "+msg);};
+
+        System.out.println("10+5="+hw.operator(10,5,addition));
+
+        greetingService1.sayMessage(",world");
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("bit");
+        list.add("world");
+        list.add("lamda");
+        list.forEach(
+		        System.out::println
+        );
+
+        /*------------------------------------*/
+
+        Integer value1 = null;
+        Integer value2 = 10;
+
+        Optional<Integer> a = Optional.ofNullable(value1);
+        Optional<Integer> b = Optional.of(value2);
+
+        System.out.println("the first args present: "+a.isPresent());
+        System.out.println("the second args present: "+b.isPresent());
+
+        int res1 = a.orElse(1);
+        int res2 = b.get();
+        System.out.println(res1+res2);
+
+    }
+
+    public void clientSocketFunc()
+    {
+        String serverName = "localhost";
+        int port = 12345;
+        try {
+            System.out.println("Connect to host: "+serverName+" , Port: "+port);
+            Socket client = new Socket(serverName,port);
+            System.out.println("Far host addr: " + client.getRemoteSocketAddress());
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+
+            out.writeUTF("hello from"+client.getLocalSocketAddress());
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+            System.out.println("Server response: "+in.readUTF());
+        } catch (IOException e) {
+	        throw new RuntimeException(e);
+        }
+    }
+
+    interface MathOperation{
+        int operation(int a, int b);
+    }
+
+    interface GreetingService{
+        void sayMessage(String msg);
+    }
+
+    private int operator(int a,int b,MathOperation mathOperation)
+    {
+        return mathOperation.operation(a,b);
     }
 }
